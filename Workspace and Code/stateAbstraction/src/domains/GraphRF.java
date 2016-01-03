c8f2451dda67e068cea8b277355070eef46cccff
dhershko <david_hershkowitz@brown.edu>
2016-01-03 17:01:24 -0500

    Changed GraphRF to use mappings from old state to new state.

--- Workspace and Code/stateAbstraction/src/domains/GraphRF.java
+++ Workspace and Code/stateAbstraction/src/domains/GraphRF.java
@@ -1,28 +1,34 @@
 package domains;
 
+import java.util.HashMap;
 import java.util.List;
 
 import burlap.domain.singleagent.graphdefined.GraphDefinedDomain;
 import burlap.oomdp.core.states.State;
+import burlap.oomdp.singleagent.Action;
 import burlap.oomdp.singleagent.GroundedAction;
 import burlap.oomdp.singleagent.RewardFunction;
 
 public class GraphRF implements RewardFunction {
 
-	List<Integer> terminalStateIDs;
+	private RewardFunction nonGraphRF;
+	private HashMap<Integer, State> graphIndexToNonGraphState;
+	private HashMap<Action, Action> graphActionToNonGraphAction;
 	
-	public GraphRF(List<Integer> terminalStateIDs) {
-		this.terminalStateIDs = terminalStateIDs;
+	public GraphRF(HashMap<Integer, State> graphIndexToNonGraphState, HashMap<Action, Action> graphActionToNonGraphAction, RewardFunction nonGraphRF) {
+		this.nonGraphRF = nonGraphRF;
+		this.graphIndexToNonGraphState = graphIndexToNonGraphState;
 	}
 	
 	@Override
 	public double reward(State s, GroundedAction a, State sprime) {
-		if(terminalStateIDs.contains(GraphDefinedDomain.getNodeId(sprime))) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
+		int stateIndex = GraphDefinedDomain.getNodeId(s);
+		int sprimeIndex = GraphDefinedDomain.getNodeId(sprime);
+		
+		return this.nonGraphRF.reward(
+				this.graphIndexToNonGraphState.get(stateIndex), 
+				this.graphActionToNonGraphAction.get(a.action).getAssociatedGroundedAction(), 
+				this.graphIndexToNonGraphState.get(sprimeIndex));
 	}
 
 }
