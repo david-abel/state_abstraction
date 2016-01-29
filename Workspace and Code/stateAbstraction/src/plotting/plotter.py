e77ee8971c6e111d6f0c6a698207aa934c69da17
Dave Abel <dave.abel.cs@gmail.com>
2016-01-29 18:07:36 -0500

    added support for displaying random policy, added legends, multiple ydata

--- Workspace and Code/stateAbstraction/src/plotting/plotter.py
+++ Workspace and Code/stateAbstraction/src/plotting/plotter.py
@@ -7,10 +7,13 @@
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+import pylab
 import sys
 import os
 from os import listdir
 from os.path import isdir, join
+
+_markers = ['v','^','s']
 
 def loadFile(taskName):
 
@@ -27,40 +30,61 @@ def parseResultsFile(resultsFile):
 	numStates = []
 	groundStateVals = []
 	abstractStateVals = []
+	rand = []
+	numGroundStates = []
 
 	for line in file(resultsFile,"r").readlines():
 		results = line.split("\t") # Tab separated result file.
+
+		if "RAND" in results:
+			# Add rand if it's in there.
+			rand.append(float(results[1]))
+			continue
+		if "NUMGROUNDSTATES" in results:
+			numGroundStates.append(int(results[1]))
+			continue
+
 		epsilons.append(float(results[0]))
 		numStates.append(int(results[1]))
 		groundStateVals.append(float(results[2]))
 		abstractStateVals.append(float(results[3]))
 
-	return epsilons, numStates, groundStateVals, abstractStateVals
+	return numGroundStates, rand, epsilons, numStates, groundStateVals, abstractStateVals
 
-def plot(xData, yData, yAxisLabel, taskName, scatter=False, xAxisLabel="Epsilon", epsilons=[]):
+def plot(xData, yData, yAxisLabel, taskName, scatter=False, xAxisLabel="Epsilon", epsilons=[], yDataNames=[]):
 	
 	if scatter:
 		# Scatter plot.
-		
-
 		for i in xrange(len(xData)):
-			plt.scatter(xData[i], yData[i], s=(epsilons[i]**4+1)*20, alpha=0.5)
+			plt.scatter(xData[i], yData[i], s=(epsilons[i]*200)/max(epsilons), alpha=0.5)
+	elif type(yData[0]) == list:
 
-		
+		for i, yDatum in enumerate(yData):
+			if len(yDataNames) >= i:
+				legendInfo = yDataNames[i]
+			else:
+				legendInfo = ""
+			plt.plot(xData, yDatum, "-", marker=_markers[i], markersize=10, label=legendInfo)
+
+		plt.xlim([min(0,min(xData)), max(xData)])
+		plt.ylim([0, max([max(y) for y in yData]) *1.2])
+
 	else: 
 		# Line plot.
-		plt.plot(xData, yData, marker="o", color="black")
+		plt.plot(xData, yData, marker="o", color="black", markersize=10)
+		plt.xlim([min(0,min(xData)), max(xData)])
+		plt.ylim([0, max(yData) + 5.0])
+
 
 	font = {'family' : 'normal',
 	        'size'   : 15}
 
 	matplotlib.rc('font', **font)
-	plt.xlim([min(0,min(xData)), max(xData)])
-	plt.ylim([0, max(yData) + 5.0])
 	plt.xlabel(xAxisLabel)
 	plt.ylabel(yAxisLabel)
 	plt.title(taskName[0].upper() + taskName[1:] + ': ' + xAxisLabel + ' vs. ' + yAxisLabel)
 	plt.grid(True)
+	plt.legend()
 	plt.savefig(taskName.lower() + '/' + xAxisLabel.lower() + '_vs_' + yAxisLabel.lower().replace(" ", "_") + '.png')
 	plt.clf()
 	# plt.show()
@@ -77,13 +101,21 @@ def makePlot(taskName):
 	resultsFile = loadFile(taskName)
 
 	# Get results.
-	epsilons, numStates, groundStateVals, abstractStateVals = parseResultsFile(resultsFile)
+	numGroundStates, randVal, epsilons, numStates, groundStateVals, abstractStateVals = parseResultsFile(resultsFile)
 
-	performances = [abs(groundStateVals[i] - abstractStateVals[i]) for i in xrange(len(groundStateVals))]
+	# randDiffs = [abs(perf - randVal[0]) for perf in groundStateVals]
+	# performances = [abs(groundStateVals[i] - abstractStateVals[i]) for i in xrange(len(groundStateVals))]
+
+	# Get the random policy value and the number of ground states.
+	randYData = randVal*len(abstractStateVals)
+	numGroundStatesYData = numGroundStates*len(numStates)
+
+
+	print "len:", len(numStates), len(numGroundStatesYData)
 
 	# Plot.
-	plot(epsilons, numStates, "Num States", taskName)
-	plot(epsilons, performances, "Abstract vs. Ground Init State Value", taskName)
+	plot(epsilons, [numStates, numGroundStatesYData], "Num Abstract States", taskName, yDataNames=["Num. Abstract States", "Num. GroundS tates"])
+	plot(epsilons, [abstractStateVals, groundStateVals, randYData], "Value of Abstract Policy Init Ground State", taskName, yDataNames=["Val. Abstract Policy", "Val. Optimal Policy", "Val. Random Policy"])
 	plot(abstractStateVals, numStates, "Num States", taskName, scatter=True, xAxisLabel="Abstract Policy Value", epsilons=epsilons)
 
 def main():
@@ -103,4 +135,6 @@ def main():
 	
 
 if __name__ == "__main__":
+	# Puts the legend into the best location in the plot.
+	pylab.rcParams['legend.loc'] = 'best'
 	main()
